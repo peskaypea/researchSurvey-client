d45834d7c171055213bb06b3d024d11cd6320f4a
Zane Reid <zanecreid@hotmail.com>
2023-01-25 07:27:10 +0000

    contact page styled and created

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,35 @@
 import React from "react";
 import Nav from "./Nav";
 import "./Contact.css";
+import contactImage from "../assets/contactform.gif";
 
 const Contact = () => {
   return (
     <div>
       <Nav />
-      <h1>Contact</h1>
+      <div className="form__container">
+        <img className="contact-gif" src={contactImage} alt="contact-img" />
+        <form>
+          <input
+            name="name"
+            type="text"
+            className="feedback-input"
+            placeholder="Name"
+          />
+          <input
+            name="email"
+            type="text"
+            className="feedback-input"
+            placeholder="Email"
+          />
+          <textarea
+            name="text"
+            className="feedback-input"
+            placeholder="Comment"
+          ></textarea>
+          <input type="submit" value="SUBMIT" />
+        </form>
+      </div>
     </div>
   );
 };
